a203df835bc96e56dfc76e7b566e8135ac0f6a93
Youngjae <limyoungjae@gmail.com>
2021-04-29 22:21:31 -0400

    Fixed a build issue with static props and made the frontend ready for the deployment to Vercel

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -47,21 +47,7 @@ export default function EventPage({ evt }) {
   )
 }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/events`)
-  const events = await res.json()
-
-  const paths = events.map((evt) => ({
-    params: { slug: evt.slug },
-  }))
-
-  return {
-    paths,
-    fallback: true,
-  }
-}
-
-export async function getStaticProps({ params: { slug } }) {
+export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
 
@@ -69,17 +55,33 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       evt: events[0],
     },
-    revalidate: 1,
   }
 }
 
-// export async function getServerSideProps({ query: { slug } }) {
-//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+// This is commented out due to build issue in the production
+// Instead, let's use getServerSideProps
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`)
+//   const events = await res.json()
+
+//   const paths = events.map((evt) => ({
+//     params: { slug: evt.slug },
+//   }))
+
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
+
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`)
 //   const events = await res.json()
 
 //   return {
 //     props: {
 //       evt: events[0],
 //     },
+//     revalidate: 1,
 //   }
 // }
